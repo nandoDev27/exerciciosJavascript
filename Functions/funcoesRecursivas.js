68a2a9ea3066df15273bb4f586c8bc3d65d103f3
nandoDev27 <luisf.dev@outlook.com>
2026-01-16 19:55:34 -0300

    Praticando funções recursivas

---
+++ Functions/funcoesRecursivas.js
@@ -0,0 +1,15 @@
+//1 - Contagem regressiva
+
+// function contagem(num){
+//     if(num === 0){
+//         console.log('Fim')
+//     } else {
+//         console.log(num)
+//        return contagem(num-1)
+//     }
+// }
+
+// contagem(10)
+
+//2 - Soma de números
+

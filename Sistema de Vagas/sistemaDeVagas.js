bbbb9b82a2013c5da1efa9a2908f9a9476d5fa02
Luis Fernando <luisf.dev@outlook.com>
2026-01-22 23:35:30 -0300

    Finalização do exercicio sistema de vagas

--- Sistema de Vagas/sistemaDeVagas.js
+++ Sistema de Vagas/sistemaDeVagas.js
@@ -41,62 +41,69 @@ do {
             break
 
         case '3':
-            alert('Em andamento...')
+            const indiceBusca = prompt('Informe o indice da vaga: ')
+
+            if (vagas.length == 0 || indiceBusca == '') {
+                alert('Vaga não encontrada, verifique se o indice está correto.')
+            } else {
+                vagas.forEach((elemento, indice) => {
+                    if (indiceBusca == indice) {
+                        alert(`Indice: ${indice} \nVaga: ${elemento.nomeVaga} \nDescrição: ${elemento.descricao}\n Data limite: ${elemento.dataLimite} \nQuantidade de inscritos: ${elemento.inscritos.length}`)
+                    }
+                })
+            }
             break
 
         case '4':
             let nomeCandidato = prompt('Informe o nome do candidato: ')
             let idadeCandidato = prompt('Informe a idade do candidato: ')
-            let indiceVaga = prompt('Informe o indice da vaga: ')
+            const indiceVaga = prompt('Informe o indice da vaga: ')
 
             confirm(`Confirmar a inscrição do candidato: ${nomeCandidato} \nIdade: ${idadeCandidato}
             \nPara a vaga n°: ${indiceVaga} ?`)
 
-            if (confirm && idadeCandidato > '18') {
-                alert('Inscrição realizada com sucesso')
+            if (confirm && idadeCandidato >= '18') {
+                vagas.forEach((vaga, indice) => {
+                    if (indiceVaga == indice) {
+                        alert('Inscrição realizada com sucesso')
+                        vaga.inscritos.push(inscreverCandidato(nomeCandidato, idadeCandidato))
+                    }
+                })
             } else if (confirm && idadeCandidato < '18') {
                 alert('Vaga não disponível para menores de idade')
             } else {
                 alert('Falha na inscrição')
             }
 
-            vagas.forEach((vaga, indice) => {
-                if (indiceVaga == indice) {
-                    vaga.inscritos.push(inscreverCandidato(nomeCandidato, idadeCandidato, indiceVaga))
-                } else if (indiceVaga != indice || indiceVaga == undefined) {
-                    alert('Vaga não encontrada, verifique se o indice está correto.')
-                } else {
-                    alert('Vaga inexistente')
-                }
-            })
             break
         case '5':
-            const indiceExcluir = prompt('Informe o indice da vaga: ')
+            let indiceExcluir = prompt('Informe o indice da vaga: ')
 
-            if (vagas == [] || indiceExcluir == '') {
+            if (vagas.length == 0 || indiceExcluir == '') {
                 alert('Não foi possível remover, verifique se o indice está correto.')
-            } else {
+            } else if (indiceExcluir) {
                 vagas.forEach((elemento, indice) => {
                     if (indiceExcluir == indice) {
-                        confirm(`Deseja excluira vaga: ${elemento.nomeVaga} \nDescrição: ${elemento.descricao} \nData limite: ${elemento.dataLimite} \nInscritos${elemento.inscritos.length}?`)
-
-                        if (confirm && indiceExcluir == indice) {
+                        confirm(`Deseja excluira vaga: ${elemento.nomeVaga} \nDescrição: ${elemento.descricao} \nData limite: ${elemento.dataLimite} \nInscritos: ${elemento.inscritos.length} ?`)
+                        if (confirm) {
                             vagas.splice(indiceExcluir, 1)
                             alert('Vaga exlcluida')
                         } else {
-                            alert('Vaga inexistente')
+                            alert('Operação cancelada!')
                         }
-                    } else if (indiceExcluir != indice) {
-                            alert('Não foi possível remover, verifique se o indice está correto.')
-                    } else {
-                        alert('Operação cancelada')
                     }
                 })
+            } else {
+                alert('Indice inválido.')
             }
+            break
 
+        case '6':
+            alert('Encerrando...')
             break
         default:
             alert('Opção inválida')
+            break
     }
 } while (opcao !== '6')
 
